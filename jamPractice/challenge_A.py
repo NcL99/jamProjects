5b96f3c7320f7dafb74826dd41199343a5a116f5
Nicholas Lloyd <33438234+NcL99@users.noreply.github.com>
2018-09-16 01:34:44 -0500

    Add files via upload

---
+++ jamPractice/challenge_A.py
@@ -0,0 +1,12 @@
+#part used for reading input to variables
+with open("file.txt") as fp: #need to edit in filename
+    T = fp.readline()
+    N = fp.readline()
+    stopsDict{}
+    while N>0:
+        stopsDict[]=
+    P =
+    interestingList = []
+    while P > 0:
+        interestingList = interestingList.append([#insert input here])
+        P--
